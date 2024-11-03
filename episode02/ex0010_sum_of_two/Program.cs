6ef517c7d6c72e2e7d3a4d4f7b04b4ecec4e540d
it0n <it.0n@mail.ru>
2024-11-03 22:23:12 +0300

    блоки кода

--- episode02/ex0010_sum_of_two/Program.cs
+++ episode02/ex0010_sum_of_two/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System.Globalization;
 
 namespace ex0010_sum_of_two // более явно определили пространство имен
-{
+{ // начало блока namespace ex0010_sum_of_two
 
     class Program
-    {
+    { // начало блока класса Program
         static void Main(string[] args)
-        {
+        { // начало блока статического метода Main
             Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16"); // устанавливаем кодировку для символов
             Console.Write("Введите первое число: ");
             var input1 = Console.ReadLine(); // здесь получаем строку в переменную input1
@@ -21,15 +21,15 @@ namespace ex0010_sum_of_two // более явно определили прос
             */
             if (double.TryParse(input1, NumberStyles.Any, CultureInfo.InvariantCulture, out double number1) &&
                 double.TryParse(input2, NumberStyles.Any, CultureInfo.InvariantCulture, out double number2))
-            {
+            { // начало блока положительной проверки оператора if
                 //double sum = number1 + number2; // закомментировали эту строку, так как в принципе она не нужна для правильной работы программы
                 Console.WriteLine($"Сумма чисел: {number1 + number2}"); // выводим результат
-            }
+            } // конец блока положительной проверки оператора if
             else
-            {   // если введенные данные не удалось преобразовать в числа, то выводим сообщение об ошибке
-                Console.WriteLine("Некорректный ввод. Пожалуйста, введите числа.");
-            }
+            { // начало блока отрицательной проверки оператора if   
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите числа."); // если введенные данные не удалось преобразовать в числа, то выводим сообщение об ошибке
+            } // конец блока отрицательной проверки оператора if 
 
-        }
-    }
-}
+        } // конец блока статического метода Main
+    } // конец блока класса Program
+} // конец блока namespace ex0010_sum_of_two
