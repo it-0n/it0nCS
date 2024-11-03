2a5fabd948ff029eead988de93d97050d98d061d
it0n <it.0n@mail.ru>
2024-11-03 21:32:54 +0300

    Пример комментариев

--- episode02/ex0010_sum_of_two/Program.cs
+++ episode02/ex0010_sum_of_two/Program.cs
@@ -1,29 +1,35 @@
 ﻿using System.Globalization;
 
-namespace ex0010_sum_of_two;
-
-class Program
+namespace ex0010_sum_of_two // боее явно определили пространство имен
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
-        Console.Write("Введите первое число: ");
-        var input1 = Console.ReadLine();
-
-        Console.Write("Введите второе число: ");
-        var input2 = Console.ReadLine();
-
-        // Преобразуем строки в числа с учетом дробной части
-        if (double.TryParse(input1, NumberStyles.Any, CultureInfo.InvariantCulture, out double number1) &&
-            double.TryParse(input2, NumberStyles.Any, CultureInfo.InvariantCulture, out double number2))
+        static void Main(string[] args)
         {
-            double sum = number1 + number2;
-            Console.WriteLine($"Сумма чисел: {sum}");
-        }
-        else
-        {
-            Console.WriteLine("Некорректный ввод. Пожалуйста, введите числа.");
-        }
+            Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16"); // устанавливаем кодировку для символов
+            Console.Write("Введите первое число: ");
+            var input1 = Console.ReadLine();
 
+            Console.Write("Введите второе число: ");
+            var input2 = Console.ReadLine();
+
+            /* 
+            Проверяем возможность преобразолвания введенных строк в числа с учетом дробной части.
+            И если преобразуется без ошибок, то производится преобразование введенных строк в числа,
+            затем вычисляется сумма этих чисел и выводится результат.
+            */
+            if (double.TryParse(input1, NumberStyles.Any, CultureInfo.InvariantCulture, out double number1) &&
+                double.TryParse(input2, NumberStyles.Any, CultureInfo.InvariantCulture, out double number2))
+            {
+                double sum = number1 + number2; // складываем числа
+                Console.WriteLine($"Сумма чисел: {sum}"); // выводим результат
+            }
+            else
+            {   // если введенные данные не удалось преобразовать в числа, выводим сообщение об ошибке
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите числа.");
+            }
+
+        }
     }
 }
