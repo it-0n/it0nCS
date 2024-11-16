0543ed40feaeb5774fc861ad1b11c239ce4c7832
it0n <it.0n@mail.ru>
2024-11-16 22:12:35 +0300

    ex0013 определение имени текущего метода

---
+++ episode02/ex0013_disable_default_namespace/Program.cs
@@ -0,0 +1,7 @@
+﻿WriteLine($"Операционная система: {System.Runtime.InteropServices.RuntimeInformation.OSDescription}");
+WriteLine($"Текущая директория: {Env.CurrentDirectory}");
+WriteLine("Имя класса: {0}", typeof(Program).Name);
+WriteLine("Имя пространства имён класса: {0}", typeof(Program).Namespace);
+WriteLine("Полное имя: {0}", typeof(Program).FullName);
+WriteLine($"Имя компьютера {Env.MachineName}");
+WriteLine($"Текущий метод: {System.Reflection.MethodBase.GetCurrentMethod()?.Name}");
