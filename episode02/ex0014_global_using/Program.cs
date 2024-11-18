b54034d03cb5bd22e48f7a30576d4309771b32ba
it0n <it.0n@mail.ru>
2024-11-18 23:51:30 +0300

    ex0014

---
+++ episode02/ex0014_global_using/Program.cs
@@ -0,0 +1,10 @@
+namespace ex0014_global_using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello from TinyConsoleTemplate with classic Main method!");
+        }
+    }
+}
