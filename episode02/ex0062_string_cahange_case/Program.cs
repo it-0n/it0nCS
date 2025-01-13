43da20af22bf65b3eb89d51897759fe314b24dee
it0n <it.0n@mail.ru>
2025-01-13 11:35:44 +0300

    ex0062 update

--- episode02/ex0062_string_cahange_case/Program.cs
+++ episode02/ex0062_string_cahange_case/Program.cs
@@ -6,19 +6,45 @@ class Program
     static void Main()
     {
         string original = "hello world!";
+        string turkishText = "Istanbul"; // Текст с латинской буквой "I"
 
-        // Преобразование в верхний регистр
+        // Преобразование в верхний регистр с учетом культуры
         string upper = original.ToUpper();
         WriteLine("Верхний регистр: " + upper);
 
-        // Преобразование в нижний регистр
+        // Преобразование в нижний регистр с учетом культуры
         string lower = upper.ToLower();
         WriteLine("Нижний регистр: " + lower);
 
-        // Преобразование в заглавные буквы каждого слова
+        // Преобразование в верхний регистр без учета культуры
+        string upperInvariant = original.ToUpperInvariant();
+        WriteLine("ToUpperInvariant: " + upperInvariant);
+
+        // Преобразование в нижний регистр без учета культуры
+        string lowerInvariant = upper.ToLowerInvariant();
+        WriteLine("ToLowerInvariant: " + lowerInvariant);
+
+        // Преобразование в нижний регистр с текущей культурой (русской)
+        string lowerCurrentCulture = turkishText.ToLower();
+        WriteLine("ToLower (русская культура): " + lowerCurrentCulture);
+
+        // Преобразование в нижний регистр с турецкой культурой
+        CultureInfo turkishCulture = new CultureInfo("tr-TR");
+        string lowerTurkishCulture = turkishText.ToLower(turkishCulture);
+        WriteLine("ToLower (турецкая культура): " + lowerTurkishCulture);
+
+        // Преобразование в нижний регистр без учета культуры (Invariant)
+        lowerInvariant = turkishText.ToLowerInvariant();
+        WriteLine("ToLowerInvariant: " + lowerInvariant);
+
+        // Преобразование в верхний регистр
+        string upperTurkishCulture = turkishText.ToUpper(turkishCulture);
+        WriteLine("ToUpper (турецкая культура): " + upperTurkishCulture);
+
+        // Преобразование каждого слова в заглавные буквы
         TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
         string titleCase = textInfo.ToTitleCase(original);
-        WriteLine("Заглавные буквы каждого слова: " + titleCase);
+        WriteLine("\nЗаглавные буквы каждого слова: " + titleCase);
 
         // Преобразование первого символа строки в верхний регистр
         if (original.Length > 0)
